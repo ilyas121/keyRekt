51314758af6ae552c699883493fd79bfafab5eb4
Ben Marshall <marsh.b.000@gmail.com>
2018-04-22 11:56:45 -0400

    Detection of a word given trace

--- src/wordMatch.py
+++ src/wordMatch.py
@@ -3,4 +3,38 @@ ENGLISH = "Dictionaries/English.txt"
 lines = list(open(ENGLISH))
 words = [word.strip() for word in lines]
 
-testTrace = "poiuytrertghnmkiuytfdsdfgyuiokn"
+# Returns true if all characters in the str1 are contained in the str2, in order.
+# For example, "pen" is embeddable in "protein", but "pit" is not, because the 'i' in protein comes after the 't'.
+def isEmbeddable(str1, str2):
+	index1 = 0
+	index2 = 0
+
+	while True:
+		if index2 >= len(str2):
+			return False
+
+		if str1[index1] == str2[index2]:
+			index1 += 1
+			if index1 >= len(str1):
+				return True
+		else:
+			index2 += 1
+
+
+# Returns the longest element in a list, according to the len function.
+def longest(lst):
+	return max(lst, key=len)
+
+def findMatch(trace):
+	possibleResults = []
+	for word in words:
+		if word[0] == trace[0] and word[-1] == trace[-1]:
+			if isEmbeddable(word, trace):
+				possibleResults.append(word)
+	possibleResults.sort(key=len, reverse=True)
+	return possibleResults
+	return str(longest(possibleResults)) + " " + str(len(possibleResults))
+
+print(len(words))
+while True:
+	print(findMatch(input()))
